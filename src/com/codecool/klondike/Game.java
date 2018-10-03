e65ffcec8f5eec402f2b4eadbac736013fcd48c7
Kovacs Reka Katalin <kovacs.reka.katalin@gmail.com>
2018-10-03 12:55:59 +0200

    refillStockFromDiscard is working

--- src/com/codecool/klondike/Game.java
+++ src/com/codecool/klondike/Game.java
@@ -2,6 +2,7 @@ package com.codecool.klondike;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ListChangeListener;
+import javafx.collections.ObservableList;
 import javafx.event.EventHandler;
 import javafx.scene.control.Alert;
 import javafx.scene.image.Image;
@@ -107,6 +108,13 @@ public class Game extends Pane {
 
     public void refillStockFromDiscard() {
         //TODO
+        ObservableList<Card> cardsDiscard = discardPile.getCards();
+        for (int i = cardsDiscard.size()-1; i > -1; i--) {
+            Card card = cardsDiscard.get(i);
+            card.flip();
+            stockPile.addCard(card);
+        }
+        discardPile.clear();
         System.out.println("Stock refilled from discard pile.");
     }
 
